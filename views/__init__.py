a053ef990c96449c35f02478ebe806340c053d1d
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-10-20 11:15:10 -0500

    Merge with main

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-from .tag_requests import get_all_tags
+from .tag_requests import get_all_tags, create_tag
 from .post_requests import get_all_posts
 from .category_requests import get_all_categories
 from .post_requests import get_all_posts, get_single_post
