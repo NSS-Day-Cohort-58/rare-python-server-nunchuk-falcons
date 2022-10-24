28642e46d8bf70598e89bff83fec0c89ed49e299
Faith Cox <fcox662@gmail.com>
2022-10-24 11:39:52 -0400

    added get single user function, updated init and handler

--- views/__init__.py
+++ views/__init__.py
@@ -2,5 +2,5 @@ from .tag_requests import get_all_tags, create_tag
 from .post_requests import get_all_posts
 from views.category_requests import get_all_categories, create_category
 from .post_requests import get_all_posts, get_single_post, create_post, update_post, delete_post
-from .user_request import get_all_users
+from .user_request import get_all_users, get_single_user
 
